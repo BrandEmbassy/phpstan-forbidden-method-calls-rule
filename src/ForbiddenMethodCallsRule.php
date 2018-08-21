83d432f9961e2b6f9d26aab84157040408be7288
Petr Hejna <Achse0@gmail.com>
2018-08-21 10:29:53 +0200

    fix namespace

--- src/ForbiddenMethodCallsRule.php
+++ src/ForbiddenMethodCallsRule.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace BrandEmbasssy\ForbiddenMethodCallsRule;
+namespace BrandEmbassy\ForbiddenMethodCallsRule;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
