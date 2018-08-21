50f91ea7b154582f4e22014eb8d613fcfdafbb6c
Petr Hejna <Achse0@gmail.com>
2018-08-21 09:45:41 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# phpstan-forbidden-method-calls-rule
+# PHPStan Forbidden Method Calls Rule
+
+> This PHPStan rule was created by Petr Morávek (https://github.com/xificurk).
+
+## Usage
+
+```bash
+composer require --dev brandembassy/phpstan-forbidden-method-calls-rule 
+```
+
+And then in your `phpstan.neon` register service:
+
+```
+services:
+    -
+        class: BrandEmbasssy\ForbiddenMethodCallsRule\ForbiddenMethodCallsRule
+        setup:
+            - addForbiddenMethod(Psr\Http\Message\StreamInterface, getContents)
+        tags:
+            - phpstan.rules.rule
+```
+
+
---
+++ src/ForbiddenMethodCallsRule.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbasssy\ForbiddenMethodCallsRule;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Type\ObjectType;
+
+class ForbiddenMethodCallsRule implements Rule
+{
+
+    /**
+     * @var ObjectType[][]
+     */
+    private $forbiddenMethods = [];
+
+    public function getNodeType(): string
+    {
+        return MethodCall::class;
+    }
+
+    public function addForbiddenMethod(string $typeName, string $methodName): void
+    {
+        $this->forbiddenMethods[$methodName][] = new ObjectType($typeName);
+    }
+
+    /**
+     * @param MethodCall $node
+     * @param Scope $scope
+     * @return string[]
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        $name = $node->name;
+        if (\is_object($name) && method_exists($name, '__toString')) {
+            $name = $name->__toString();
+        }
+
+        if (!\is_string($name)) {
+            return [];
+        }
+
+        $forbiddenTypes = $this->forbiddenMethods[$name] ?? [];
+
+        foreach ($forbiddenTypes as $forbiddenType) {
+            $type = $scope->getType($node->var);
+            if ($forbiddenType->isSuperTypeOf($type)->yes()) {
+                return [
+                    sprintf(
+                        'Calling forbidden method %s:%s().',
+                        $forbiddenType->getClassName(),
+                        $name
+                    ),
+                ];
+            }
+        }
+
+        return [];
+    }
+
+}
